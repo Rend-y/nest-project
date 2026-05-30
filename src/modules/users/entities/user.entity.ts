5cf503db5fc8aab7380196d15d459927114360d7
Rendy <wladimi482002@gmail.com>
2026-05-30 20:24:15 +0300

    add: user database

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+@Unique('UQ_users_username_email', ['username', 'email'])
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  username: string;
+
+  @Column({ type: 'integer' })
+  age: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  email: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date | null;
+}
---
+++ src/modules/users/repository/users.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { UserEntity } from '../entities/user.entity';
+
+@Injectable()
+export class UsersRepository extends Repository<UserEntity> {
+  constructor(private readonly dataSource: DataSource) {
+    super(UserEntity, dataSource.createEntityManager());
+  }
+}
